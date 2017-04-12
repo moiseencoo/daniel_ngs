eedacd0e15e495ff4d8d7ed57b55b395b7324ac1
moiseencoo <95varya@mail.ru>
2017-04-12 18:07:06 +0500

    Page Added: invenio-project

--- js/main.js
+++ js/main.js
@@ -7,13 +7,13 @@ $(document).ready(function(){
 
  $(".header-menu a").on('click', function(){
         
-       $('#navigation').animate({left: '0'}, "slow");
+       $('#navigation').animate({left: '0'}, "0");
      
         $(".header-menu a").hide();
 });
 
  $(".close_menu").on('click', function(){
         
-         $('#navigation').animate({left: '-400px'}, "slow");
+         $('#navigation').animate({left: '-400px'}, "0");
          $(".header-menu a").show();
 });
